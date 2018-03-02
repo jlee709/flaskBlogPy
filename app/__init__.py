2a0bcbeb2c1ccb2e5710132910fb2fb35b3caa3d
Justin Lee <leejwg.3@gmail.com>
2018-03-02 18:25:21 +0900

    started building template forms using wtforms

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 # creates application object as an instance of Flask Class
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes
