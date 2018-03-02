ea2f229b82c871a85782624e66a6052dd4692aeb
Justin Lee <leejwg.3@gmail.com>
2018-03-02 17:35:07 +0900

    using templates for user,body,posts, can render to page

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return 'hello fucker'
-
-  
+    # sets use for tempaltes in template file and calls using render_
+    user = {'username': 'Justin'}
+    posts =[
+        {
+        'author':{'username': 'Winky'},
+        'body': 'Seoul is a beauty city'
+        },
+        {
+        'author': {'username': ' Wayne'},
+        'body': 'The origin or Karct-do'
+        }
+    ]
+    return render_template('index.html', title="Home", user=user, posts=posts)
